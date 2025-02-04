70656a7b4da2defc3e6fc3561255005ede540a17
Polosq <paadurlien@gmail.com>
2025-02-04 19:41:18 +0100

    kaka?

--- placeholder.js
+++ placeholder.js
@@ -1,7 +1,3 @@
-divcursor = document.getElementById("redirectioncursor");
-divaudio = document.getElementById("redirectionaudio");
-divembed = document.getElementById("redirectionembed");
-
 let tableau = [];
 
 
