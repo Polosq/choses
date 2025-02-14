a13c239c0804de113082f2fb673f27ee5632384c
Polosq <paadurlien@gmail.com>
2025-02-14 22:57:30 +0100

    a

--- placeholder.js
+++ placeholder.js
@@ -24,7 +24,6 @@ function newtext(e) {
     var ul = document.createElement("ul");
     var li = document.createElement("li");
     var a = document.createElement("a");
-    li.appendChild(text);
     ul.appendChild(li);
 
     a.href = url;
