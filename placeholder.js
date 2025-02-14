832c36fa83764b4128424fe97ac2b89b314a82c2
Polosq <paadurlien@gmail.com>
2025-02-14 21:57:32 +0100

    aa

--- placeholder.js
+++ placeholder.js
@@ -1,3 +1,21 @@
+let bartxt = document.getElementById("searchbar");
+let barurl = document.getElementById("inputweb");
+
+let btn = document.getElementById("searchbutton");
+
 let tableau = [];
 
+function newtext(e) {
+    let txt = bartxt.value;
+    let url = barurl.value;
+    tableau.push({
+        text: txt,
+        url: url
+    });
+    console.log(tableau);
+    bartxt.value = "";
+    barurl.value = "";
+    e.preventDefault();
+}
 
+btn.addEventListener("click", )
