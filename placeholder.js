d02992eade3a8bff7eb50994685d4a08293747d0
Polosq <paadurlien@gmail.com>
2025-02-14 23:18:41 +0100

    a

--- placeholder.js
+++ placeholder.js
@@ -16,23 +16,29 @@ function newtext(e) {
     console.log("click");
     let txt = bartxt.value;
     let url = barurl.value;
-    result = txt + " " + url;
-    console.log(result);
     bartxt.value = "";
     barurl.value = "";
-
-    console.info(txt, url)
     
     var ul = document.createElement("ul");
     var li = document.createElement("li");
     var a = document.createElement("a");
-    div_reponse.appendChild(ul);
-    ul.appendChild(li);
-
-    a.href = url;
-    a.value = txt;
     
-    li.appendChild(a);
+    if(url==""){
+
+    }else if (txt=""){
+
+    }else {
+        div_reponse.appendChild(ul);
+        ul.appendChild(li);
+
+        a.href = url;
+        a.value = txt;
+    
+        li.appendChild(a);
+        a.appendChild(txt);
+    }
+
+    
     e.preventDefault();
 }
 
