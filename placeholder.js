28eacf79f2f1473072b063cecf7e5dfdbf81bc43
Polosq <paadurlien@gmail.com>
2025-02-14 22:53:09 +0100

    a

--- placeholder.js
+++ placeholder.js
@@ -3,22 +3,34 @@ let barurl = document.getElementById("inputweb");
 
 let btn = document.getElementById("searchbutton");
 
-let tableau = [];
+let div_reponse = document.getElementById("zonedereponse");
+
+let result="";
 
 bartxt.value = "";
 barurl.value = "";
+
+div_reponse.innerHTML = "";
 
 function newtext(e) {
     console.log("click");
     let txt = bartxt.value;
     let url = barurl.value;
-    tableau.push({
-        text: txt,
-        url: url
-    });
-    console.log(tableau);
+    result = txt + " " + url;
+    console.log(result);
     bartxt.value = "";
     barurl.value = "";
+    
+    var ul = document.createElement("ul");
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    li.appendChild(text);
+    ul.appendChild(li);
+
+    a.href = url;
+    a.value = txt;
+    
+    div_reponse.appendChild(a);
     e.preventDefault();
 }
 
