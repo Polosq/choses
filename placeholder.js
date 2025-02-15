5358874d6266074312a393b539a7dee4f9fe9f66
Polosq <paadurlien@gmail.com>
2025-02-15 16:28:56 +0100

    a

--- placeholder.js
+++ placeholder.js
@@ -25,7 +25,7 @@ function newtext(e) {
     if(url!=""){
         if (txt!=""){
             if (compteur > 15){
-                alert("Vous ne pouvez pas mettre plus de 15 liens !")                
+                alert("Vous ne pouvez pas mettre plus de 15 liens !");                
             }else {
                 div_reponse.appendChild(li);
 
@@ -37,7 +37,7 @@ function newtext(e) {
 
                 li.appendChild(a);
                 a.appendChild(texte);
-                compteur++
+                compteur++;
             }
         }
     }
