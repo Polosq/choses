04e0238f763b9e9587e593ad72e3f35ff1a459f9
Polosq <paadurlien@gmail.com>
2025-02-17 13:53:57 +0100

    a

--- placeholder.js
+++ placeholder.js
@@ -68,7 +68,7 @@ function deleteimg(e){
 }
 
 function actualier(e){
-    var texte = document.createTextNode("Nombre total de liens : "+ count + "/15")
+    var texte = "Nombre total de liens : "+ count + "/15"
     nblien.innerHTML=texte;
 }
 
