6effbbabe0b8fa84cb18457abfdfdbbc2b984127
Polosq <paadurlien@gmail.com>
2025-02-25 19:07:53 +0100

    a

--- tuto.js
+++ tuto.js
@@ -1,20 +1,37 @@
-let closecursor = document.getElementById("closecursor");
 let boutoncursor = document.getElementById("tutocursor");
+
+let boutonextension = document.getElementById("tutoextension");
 
 boutoncursor.addEventListener('click', function(){
     var divtutocursor = document.getElementById("divtutocursor");
+    var closecursor = document.getElementById("closecursor");
     if (divtutocursor.style.display == "none") {
         divtutocursor.style.display = "block";
         closecursor.addEventListener('click', function(){
             divtutocursor.style.display = "none";
         });
-        var closecursor = document.getElementById("closecursor");
+        
         closecursor.addEventListener('click', function(){
             divtutocursor.style.display = "none";
         });
     } else {
         divtutocursor.style.display = "none";
-    }
-    
+    } 
 });
 
+boutonextension.addEventListener('click', function(){
+    var divtutoextension = document.getElementById("divtutoextension");
+    var closeextension = document.getElementById("closeextension");
+    if (divtutoextension.style.display == "none") {
+        divtutoextension.style.display = "block";
+        closeextension.addEventListener('click', function(){
+            divtutoextension.style.display = "none";
+        });
+        
+        closeextension.addEventListener('click', function(){
+            divtutoextension.style.display = "none";
+        });
+    } else {
+        divtutoextension.style.display = "none";
+    } 
+});
