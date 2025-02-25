9b808d7de786500d7afc12e799518fecee3fa370
Polosq <paadurlien@gmail.com>
2025-02-25 18:33:49 +0100

    a

--- tuto.js
+++ tuto.js
@@ -1,15 +1,20 @@
 let closecursor = document.getElementById("closecursor");
-let divtutocursor = document.getElementById("divtutocursor");
 let boutoncursor = document.getElementById("tutocursor");
 
 boutoncursor.addEventListener('click', function(){
+    var divtutocursor = document.getElementById("divtutocursor");
     if (divtutocursor.style.display == "none") {
         divtutocursor.style.display = "block";
+        closecursor.addEventListener('click', function(){
+            divtutocursor.style.display = "none";
+        });
+        var closecursor = document.getElementById("closecursor");
+        closecursor.addEventListener('click', function(){
+            divtutocursor.style.display = "none";
+        });
     } else {
         divtutocursor.style.display = "none";
     }
+    
 });
 
-closecursor.addEventListener('click', function(){
-    divtutocursor.style.display = "none";
-});
