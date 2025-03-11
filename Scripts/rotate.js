84988e229ff8f26ce96fe048f61dd8e843c995dc
Polosq <paadurlien@gmail.com>
2025-03-11 20:38:22 +0100

    a

--- Scripts/rotate.js
+++ Scripts/rotate.js
@@ -4,7 +4,7 @@ let bouton = document.getElementById('rotatebutton');
 bouton.addEventListener('click', function() {
     if(iframeEE.height='400px'){
         iframeEE.height='600px';
-        iframeEE.width='250px';
+        iframeEE.width='400px';
     }else{
         iframeEE.height='400px';
         iframeEE.width='100%';
