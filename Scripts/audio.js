85ca7f508737684a5f63ccfab4e3ab77df7c0471
Polosq <paadurlien@gmail.com>
2025-03-12 17:43:42 +0100

    a

--- Scripts/audio.js
+++ Scripts/audio.js
@@ -21,26 +21,26 @@ boutonmic.addEventListener('click', async () => {
                 boutonmic.style.display="block";
                 boutonmic.innerHTML = "Arrêter l'enregistrement ..."; 
                 clearInterval(countdownInterval);
+                const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+                mediaRecorder = new MediaRecorder(stream);
+                mediaRecorder.start();
+
+                mediaRecorder.ondataavailable = (event) => {
+                    if (event.data.size > 0) {
+                        recordedChunks.push(event.data);
+                    }
+                };
+
+                mediaRecorder.onstop = () => {
+                    const blob = new Blob(recordedChunks, { type: 'audio/mp3' });
+                    const url = URL.createObjectURL(blob);
+                    document.getElementById('audiomicro').src = url;
+                    document.getElementById('microdownload').download = url;
+                    recordedChunks = [];
+                };
             }
         }, 1000);
 
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.start();
-
-        mediaRecorder.ondataavailable = (event) => {
-            if (event.data.size > 0) {
-                recordedChunks.push(event.data);
-            }
-        };
-
-        mediaRecorder.onstop = () => {
-            const blob = new Blob(recordedChunks, { type: 'audio/mp3' });
-            const url = URL.createObjectURL(blob);
-            document.getElementById('audiomicro').src = url;
-            document.getElementById('microdownload').href = url;
-            recordedChunks = [];
-        };
     }else{
         mediaRecorder.stop();
         div.style.backgroundColor = "rgba(255,0,0,0.5)";
