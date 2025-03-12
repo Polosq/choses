5fe714199133e40c6c44f9e094b50374d07581f6
Polosq <paadurlien@gmail.com>
2025-03-12 14:17:15 +0100

    a

--- Scripts/rotate.js
+++ Scripts/rotate.js
@@ -5,8 +5,20 @@ bouton.addEventListener('click', function() {
     if(iframeEE.height=='400px'){
         iframeEE.height='600px';
         iframeEE.width='400px';
+
+        if(iframeinput.value ==""){
+            divtutoextension.style.top = "750px";
+        }else {
+            divtutoextension.style.top = "1100px";
+        }
+
     }else if(iframeEE.height=='600px'){
         iframeEE.height='400px';
         iframeEE.width='100%';
+        if(iframeinput.value ==""){
+            divtutoextension.style.top = "550px";
+        }else {
+            divtutoextension.style.top = "900px";
+        }
     }
 });
