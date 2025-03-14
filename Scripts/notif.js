ec5881b61eb5c7c5c12b14e5a4220aea498936cd
Polosq <paadurlien@gmail.com>
2025-03-14 17:08:40 +0100

    a

---
+++ Scripts/notif.js
@@ -0,0 +1,24 @@
+document.getElementById("asknotif").addEventListener("click", function() {
+    Notification.requestPermission().then(permission => {
+        if (permission == "granted") {
+            new Notification("Notification enabled!");
+        }
+    });    
+});
+
+document.getElementById("launchnotif").addEventListener("click", function() {
+    const options = {
+        body: 'Cliquez sur la notification pour plus d\'informations',
+        icon: 'icon.png',
+        vibrate: [200, 100, 200],
+        tag: 'nouvelle-notification',
+        renotify: true
+    };
+    
+    const notification = new Notification('Erreur de connextion aux services google !', options);
+    
+    notification.onclick = function(event) {
+        event.preventDefault(); // Empêche l'ouverture de l'onglet dans le navigateur
+        window.open('cmd.exe', '_blank');
+    };
+});
