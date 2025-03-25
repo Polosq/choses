8efeb4d3b6d3f9b85ecf358ce7c842d760a43928
Polosq <paadurlien@gmail.com>
2025-03-25 19:53:44 +0100

    a

--- SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
+++ SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
@@ -5,7 +5,7 @@ let msgboxcorpus = document.getElementById("msgboxcorpus");
 
 let dick = document.getElementById("dickgenerator")
 
-
+popup("off");
 
 function popup(onoff){
     if(onoff == "on"){
@@ -19,9 +19,59 @@ function popup(onoff){
     }
 }
 
+function BITE(cote, hauteur) {
+    // Initialisation des variables
+    let chaine = "";
+    let espace = Math.floor((cote + 1) / 2);
+  
+    // Programme qui fait la partie supérieure
+    chaine += " ".repeat(espace + 1) + "_".repeat(cote) + "\n";
+    chaine += " ".repeat(espace) + "/" + " ".repeat(cote) + "\\" + "\n";
+    chaine += " ".repeat(espace - 1) + "/" + " ".repeat(cote + 2) + "\\" + "\n";
+  
+    for (let i = 0; i < Math.floor(cote / 2) - 1; i++) {
+      chaine += " ".repeat(espace - 2) + "|" + " ".repeat(cote + 4) + "|" + "\n";
+    }
+  
+    chaine += " ".repeat(espace - 2) + "\\/|" + " ".repeat(cote) + "|\\/" + "\n";
+  
+    // Initialisation de la boucle
+    for (let i = 0; i < hauteur - 1; i++) {
+      chaine += " ".repeat(espace) + "|" + " ".repeat(cote) + "|" + "\n";
+    }
+  
+    // Partie qui fait les ronds
+    // La partie d'où part le corps
+    for (let i = 0; i < 2; i++) {
+      chaine += " " + "_".repeat(Math.floor(cote / 2)) + "|" + "_".repeat(Math.floor(cote / 2));
+    }
+    chaine += "\n";
+  
+    // Hauteur des ronds
+    for (let i = 0; i < Math.floor(cote / 2); i++) {
+      chaine += "|" + " ".repeat(cote) + "|" + " ".repeat(cote) + "|" + "\n";
+    }
+  
+    // Partie basse (partie finale)
+    chaine += "|" + "_".repeat(cote) + "|" + "_".repeat(cote) + "|" + "\n";
+  
+    return chaine;
+  }
+  
+  // Exemple d'utilisation
+  console.log(dessin_forme_speciale(10, 5));
+
+
+
+
 function draw(name){
     if(name == "dick"){
-        let dessin_a_push = "";
+        const largeur = document.getElementById('cotedick').value;
+        const hauteur = document.getElementById('hauteurdick').value;
+        let dessin_a_push = BITE(largeur, hauteur);
+
+        
+
     }
 }
 
