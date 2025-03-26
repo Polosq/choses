cebed49d9fa74d532d89512fe9340e6b4d03e4ee
Polosq <paadurlien@gmail.com>
2025-03-26 16:06:00 +0100

    a

--- SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
+++ SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
@@ -31,31 +31,31 @@ function BITE(cote, hauteur) {
     let espace = Math.floor(Math.floor((Math.floor((cote + 1) / 2)-1)/9) - correction);
   
     // Programme qui fait la partie supérieure
-    chaine += " ".repeat(espace + 1) + "_".repeat(cote) + "\n";
-    chaine += " ".repeat(espace) + "/" + " ".repeat(cote) + "\\" + "\n";
-    chaine += " ".repeat(espace - 1) + "/" + " ".repeat(cote) + " ".repeat(2) + "\\" + "\n";
+    chaine += caractere.repeat(espace) + caractere + "_".repeat(cote) + caractere + caractere.repeat(espace) + "\n";
+    chaine += caractere.repeat(espace) + "/" + " ".repeat(cote) + "\\" + caractere.repeat(espace) + "\n";
+    chaine += caractere.repeat(espace - 1) + "/" + caractere.repeat(cote) + caractere.repeat(2) + "\\" + caractere.repeat(espace-1) + "\n";
   
     for (let i = 0; i < Math.floor(cote / 2) - 1; i++) {
-      chaine += " ".repeat(espace - 2) + "|" + " ".repeat(cote) + " ".repeat(4) + "|" + "\n";
+      chaine += caractere.repeat(espace - 2) + "|" + caractere.repeat(cote) + caractere.repeat(4) + "|" + caractere.repeat(espace - 2) + "\n";
     }
   
-    chaine += " ".repeat(espace - 2) + "\\/|" + " ".repeat(cote) + "|\\/" + "\n";
+    chaine += caractere.repeat(espace - 2) + "\\/|" + caractere.repeat(cote) + "|\\/" + caractere.repeat(espace - 2) + "\n";
   
     // Initialisation de la boucle
     for (let i = 0; i < hauteur - 1; i++) {
-      chaine += " ".repeat(espace) + "|" + " ".repeat(cote) + "|" + "\n";
+      chaine += caractere.repeat(espace) + "|" + caractere.repeat(cote) + "|" + caractere.repeat(espace) + "\n";
     }
   
     // Partie qui fait les ronds
     // La partie d'où part le corps
     for (let i = 0; i < 2; i++) {
-      chaine += " " + "_".repeat(Math.floor(cote / 2)) + "|" + "_".repeat(Math.floor(cote / 2));
+      chaine += caractere + "_".repeat(Math.floor(cote / 2)) + "|" + "_".repeat(Math.floor(cote / 2)) + caractere;
     }
     chaine += "\n";
   
     // Hauteur des ronds
     for (let i = 0; i < Math.floor(cote / 2); i++) {
-      chaine += "|" + " ".repeat(cote) + "|" + " ".repeat(cote) + "|" + "\n";
+      chaine += "|" + caractere.repeat(cote) + "|" + caractere.repeat(cote) + "|" + "\n";
     }
   
     // Partie basse (partie finale)
@@ -67,12 +67,12 @@ function BITE(cote, hauteur) {
 
 
 function draw(name){
-    if(name == "dick"){
-        const largeur = document.getElementById('cotedick').value;
-        const hauteur = document.getElementById('hauteurdick').value;
+  if(name == "dick"){
+      const largeur = document.getElementById('cotedick').value;
+      const hauteur = document.getElementById('hauteurdick').value;
         
-        var dessin_a_push = BITE(largeur, hauteur);
-    }
+      var dessin_a_push = BITE(largeur, hauteur);
+  }
 
     msgboxcorpus.innerHTML = `<pre>${dessin_a_push}</pre>`;
 }
@@ -80,4 +80,14 @@ function draw(name){
 dick.addEventListener("click", function(){
     popup("on");
     draw("dick");
+});
+
+document.querySelector("form").addEventListener("submit", function(){
+  const boutonsRadio = document.querySelectorAll('input[name="options"]');
+  var caractere = " ";
+  boutonsRadio.forEach(bouton => {
+    if (bouton.checked) {
+      caractere = bouton.value;
+    }
+  })
 });
