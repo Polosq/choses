fb812a347d11014857c781cb63481e95a512d7ab
Polosq <paadurlien@gmail.com>
2025-03-26 10:38:06 +0100

    a

--- SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
+++ SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
@@ -21,7 +21,7 @@ function popup(onoff){
 function BITE(cote, hauteur) {
     // Initialisation des variables
     let chaine = "";
-    let espace = (Math.floor((cote + 1) / 2)-1)/6;
+    let espace = (Math.floor((cote + 1) / 2)-1)/9;
   
     // Programme qui fait la partie supérieure
     chaine += " ".repeat(espace + 1) + "_".repeat(cote) + "\n";
