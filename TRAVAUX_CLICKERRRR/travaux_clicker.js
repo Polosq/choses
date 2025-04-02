71210d885c412e9536a1a13b60f71b0e244316d7
Polosq <paadurlien@gmail.com>
2025-04-02 22:17:42 +0200

    a

--- TRAVAUX_CLICKERRRR/travaux_clicker.js
+++ TRAVAUX_CLICKERRRR/travaux_clicker.js
@@ -101,8 +101,7 @@ function upgrades_actualiser(){
             else{
                 // faire une animation en rouge pour dire que pas assez de crédits
                 
-            }
-            
+            } 
         });
     }
 }
