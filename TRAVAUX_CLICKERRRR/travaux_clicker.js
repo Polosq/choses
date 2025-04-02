b285514de6177bd43dd0b5f1eafed1077191fd3c
Polosq <paadurlien@gmail.com>
2025-04-02 22:01:26 +0200

    commiiittttteeeeeeeee

--- TRAVAUX_CLICKERRRR/travaux_clicker.js
+++ TRAVAUX_CLICKERRRR/travaux_clicker.js
@@ -102,7 +102,6 @@ function upgrades_actualiser(){
             }
             else{
                 // faire une animation en rouge pour dire que pas assez de crédits
-                
             }
             
         });
