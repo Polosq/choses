0616d69d66bc7626a8974d21812617521bc99a0c
Polosq <paadurlien@gmail.com>
2025-04-02 21:32:39 +0200

    a

--- TRAVAUX_CLICKERRRR/travaux_clicker.js
+++ TRAVAUX_CLICKERRRR/travaux_clicker.js
@@ -35,12 +35,13 @@ function upgrades_actualiser(){
     for (let i=1; i<upgrades_debloquees.length; i++){
         
         // Tests pour savoir si l'amélioration est débloquée
-        console.error(upgrades_debloquees[i-1]);
         if(upgrades_debloquees[i] == "unlocked"){
             upgraded = true;
         }else if(upgrades_debloquees[i] == "locked"){
             upgraded = false;
         }
+
+        console.info(upgraded);
 
         // Création de la boite d'amélioration
 
