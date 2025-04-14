d6e7d0f32e0d1aa103099368491567a7921275f1
Polosq <paadurlien@gmail.com>
2025-04-14 17:58:52 +0200

    a

--- SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
+++ SΣ⊂ℝ€t/SΣ⊂ℝ€t_∂χρ∈⊥θχ.js
@@ -592,7 +592,7 @@ window.addEventListener("scroll", function () {
   var maDiv = document.getElementById("separator_selector");
   if (window.scrollY > 665) {
       maDiv.style.position = "absolute";
-      maDiv.style.top = "690px";
+      maDiv.style.top = "687px";
   } else {
       maDiv.style.position = "fixed";
       maDiv.style.top = "25px";
